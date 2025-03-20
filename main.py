92bb592dfff43c1e232125ba82fd4b39a5a831ca
Andrew Petrulakis <petrulakisandrew@gmail.com>
2025-03-20 15:45:44 -0500

    created test data frame

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import os
 import pandas as pd 
+
+test_data = {
+    'Name': ['Tenant1','Tenant2','Tenant3','Tenant4'],
+    'Caseworker': ['Caseworker1','Caseworker2','Caseworker3','Caseworker4'],
+    'City': ['City1','City2','City3','City4']
+}
+
+# print(test_data)
+
+test_df = pd.DataFrame(test_data, columns = ['Name','Caseworker','City'])
+
+print(test_df)
